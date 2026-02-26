679969a3c574c5ca708ca8c8228d17c3aedf6dc8
Ana Paula <paulinha2903@live.com>
2026-02-25 23:06:24 -0300

    ✔ Sistema de autenticação ✔ Proteção de rotas ✔ CRUD completo ✔ Controle de sessão ✔ Integração com banco

--- app.py
+++ app.py
@@ -1,46 +1,159 @@
-from flask import Flask, request , jsonify # Importação da biblioteca Flask
-from models.user import User
-from database import db
-from flask_login import LoginManager, login_user, current_user
 
-app = Flask(__name__) # cria aplicação
-app.config['SECRET_KEY'] = "sua_chave_secreta" # 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' # caminho para conexao do banco
+# IMPORTAÇÕES
+from flask import Flask, request , jsonify # Framework Flask + manipulação de requisição e resposta JSON
+# Flask → cria a aplicação web
+# request → pega dados enviados pelo cliente (Postman, navegador)
+#jsonify → transforma dicionário Python em resposta JSON
 
-login_Manager = LoginManager()
-db.init_app(app)
-login_Manager.init_app(app)
+from models.user import User # Modelo da tabela User (ORM) 
+#Importa seu modelo de usuário (classe que representa a tabela no banco).
 
-login_Manager.login_view = 'login'
+from database import db # Importa o objeto do SQLAlchemy, que controla o banco de dados.
 
-# cria rota
+# Sistema de autenticação e sessão
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required 
+# LoginManager → gerencia autenticação
+# login_user() → faz login do usuário
+# current_user → usuário logado
+# logout_user() → encerra sessão
+# login_required → protege rotas
+
+
+# Apenas para confirmar que o arquivo está sendo executado
+print("APP INICIANDO...")
+
+# CRIAÇÃO DA APLICAÇÃO
+
+# Cria a aplicação Flask
+app = Flask(__name__) 
+
+# Chave secreta usada para proteger sessão e cookies
+app.config['SECRET_KEY'] = "sua_chave_secreta" 
+
+# Configuração do banco de dados (SQLite local)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' 
+
+
+# CONFIGURAÇÃO DO LOGIN E BANCO
+login_manager = LoginManager() # Cria gerenciador de login
+
+db.init_app(app) # Conecta o banco à aplicação
+login_manager.init_app(app) # Conecta o sistema de login à aplicação
+
+# Define para qual rota o usuário será redirecionado se não estiver logado
+login_manager.login_view = 'login'
+
+# FUNÇÃO OBRIGATÓRIA DO FLASK-LOGIN
+# CARREGAR USUÁRIO LOGADO
+@login_manager.user_loader
+def load_user(user_id):
+
+    # Essa função é chamada automaticamente pelo Flask-Login. Ela recebe o ID salvo na sessão e retorna o usuário correspondente no banco.
+    return User.query.get(user_id)
+
+# ROTAS
+# LOGIN
 @app.route('/login', methods=['POST'])
-def login(user):
-    data = request.json
-    username = data.get("username")
-    password = data.get("password")
+def login():
+    
+    # Recebe username e password via JSON Verifica no banco se e válido, cria sessão de login.
 
+    data = request.get_json() # Pega o JSON enviado pelo Postman.
+
+    username = data.get("username") # Extrai dados do JSON.
+    password = data.get("password") #-----------------
+    
     if username and password:
-        #login
+
+        # Busca usuário no banco pelo username
         user = User.query.filter_by(username=username).first()
 
-# funcao de verificacao se a senha esta correta 
+        # Verifica se usuário existe e senha confere
         if user and user.password == password:
-            login_user(user)
+            login_user(user) # Cria sessão
             print(current_user.is_authenticated)
-            return jsonify({"Menssagem": "Autencicação realizada com sucesso"})
+            return jsonify({"Menssagem": "Autenticacao realizada com sucesso"}), 200
     
     return jsonify({"Menssagem": "Credenciais Invalida"}), 400
 
+# LOGOUT
+@app.route('/logout', methods=['GET'])
+@login_required # Só permite acesso se estiver logado
+def logout():
 
-@app.route("/")
-def home():
-    return "Página inicial"
+    # Encerra sessão do usuário atual
+    logout_user() # remove sessão.
+    return jsonify({"menssagem": "Logout realizado com sucesso"})
 
-@app.route("/hello-world", methods = ["GET"])
-def hello_word():
-    return "Hello World"
+# CRIAR USUÁRIO (CREATE)
+@app.route('/user', methods=['POST'])
+@login_required
+def create_user():
+
+    # Cria novo usuário no banco
+    data = request.json
+    username = data.get("username") # Extrai dados do JSON.
+    password = data.get("password") # ////////////////////
+
+    if username and password:
+
+        # Cria objeto User
+        user = User (username=username, password=password)
+        db.session.add(user) # Cria objeto User
+        db.session.commit()  # Salva no banco de dados
+        return jsonify({"Menssagem": "Usuario cadastrado com sucesso"})
+
+    return jsonify ({"Menssagem": "Dados Invalidos"}), 400
+
+# LER USUÁRIO (READ) 
+@app.route('/user/<int:id_user>', methods=['GET']) # <int:id_user> = parâmetro na URL.
+@login_required
+def read_user(id_user):
+
+    # Busca usuário pelo ID
+    user = User.query.get(id_user)
+
+    if user:
+        return {"username": user.username}
+    
+    return jsonify ({"menssagem": "Usuario nao encontrado"}), 400
+
+# ATUALIZAR USUÁRIO (UPDATE)
+@app.route('/user/<int:id_user>', methods=['PUT'])
+@login_required
+def update_user(id_user):
+
+    # Atualiza senha do usuário
+    data = request.json
+    user = User.query.get(id_user)
+
+    if user and data.get("password"):
+        user.password = data.get("password")
+        db.session.commit()
+
+        return jsonify ({"Menssagem": f"Usuario {id_user} atualizado com sucesso"})
+    
+    return jsonify ({"menssagem": "Usuario nao encontrado"}), 400
+
+# DELETAR USUÁRIO (DELETE)
+@app.route('/user/<int:id_user>', methods=['DELETE'])
+@login_required
+def delete_user(id_user):
+    user = User.query.get(id_user)
+
+    # Impede que usuário delete a si mesmo
+    if id_user == current_user.id:
+        return jsonify ({"Menssagem": "Delecao nao permitida"}), 403
+    
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({"Menssagem": f"Usuario {id_user} deletado com sucesso"})
+    
+    return jsonify ({"menssagem": "Usuario nao encontrado"}), 400
+
+
 
 # verifica se esta sendo executado 
-if __name__ == '_main_':
+if __name__ == "__main__":
     app.run(debug=True)
